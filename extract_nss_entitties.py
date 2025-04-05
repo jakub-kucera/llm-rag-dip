8ea2c762c971d7aac838e12f89b420d518247697
Jakub Kucera <jakub.kucerja@gmail.com>
2025-04-05 11:12:59 +0200

    Improve extract nss entities script

--- extract_nss_entitties.py
+++ extract_nss_entitties.py
@@ -1,96 +1,127 @@
-import json
+import getpass
 import os
-from vllm import LLM, SamplingParams
+import json
+import datetime
+from math import ceil
+from time import sleep
 
-# input_dir = "files20232024"
-input_dir = "/home/kucerj56/llm-rag-dip/files_nss"
-output_file = "/home/kucerj56/llm-rag-dip/preprocessed_nss.jsonl"
+from langchain_google_genai import ChatGoogleGenerativeAI
+from tqdm import tqdm
 
-output_dataset = []
-# for c, file in enumerate(os.listdir(input_dir)):
-#     print(f"Opening {file}...")
-#     with open(f"{input_dir}/{file}") as f:
-#         content = f.read()
-        # print(content)
+# CONTEXXT_MAX_LENGTH = 128000
+CONTEXXT_MAX_LENGTH = 900000
 
-MAX_MODEL_LEN = 128000
-# llm = LLM(model="/home/kucerj56/models/mistralai/Ministral-8B-Instruct-2410")  # Replace with your actual model path
-llm = LLM(
-    model="/home/kucerj56/models/microsoft/Phi-4-mini-instruct",
-    # model="/home/kucerj56/models/meta-llama/Llama-3.1-8B-Instruct",
-    # model="/home/kucerj56/models/meta-llama/Llama-3.3-70B-Instruct-bnb-4bit",
-    # quantization="bitsandbytes",
-    # load_format="bitsandbytes",
-    max_logprobs = 32016,
-    max_model_len = MAX_MODEL_LEN,
-    # max_model_len = 80000,
-    trust_remote_code=True
-)
-sampling_params = SamplingParams(
-    temperature=0.1,
-    max_tokens=2048,
+def process_nss_documents(input_dir: str, output_file: str):
+    # These are now passed as function arguments
+    output_dataset = []
+    # for c, file in enumerate(os.listdir(input_dir)):
+    #     print(f"Opening {file}...")
+    #     with open(f"{input_dir}/{file}") as f:
+    #         content = f.read()
+    #         print(content)
+
+    if "GOOGLE_API_KEY" not in os.environ:
+        os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash",
+        temperature=0.1,
+        # max_tokens=2048,
+        max_tokens=8192,
+        max_retries=2
     )
 
-prompt_template = """
-Below is a document which is a “rozhodnutí Nejvyššího správního soudu”.
+    prompt_template = """
+    Below is a document which is a “rozhodnutí Nejvyššího správního soudu”.
 
-[START OF DOCUMENT]
-{document_contents}
-[END OF DOCUMENT]
+    [START OF DOCUMENT]
+    {document_contents}
+    [END OF DOCUMENT]
 
-Carefully analyse it and answer the following questions.
-1. what was the verdict
-2. is there a right to compensation of legal expenses
-3. is there a possibility of appeal
-4. list of referenced paragraphs
-5. list of referenced entities
-Output the answer as a single valid JSON only, do not output any other text or characters. Example of output:
-{{
-     "verdict": "žaloba odmítnuta",
-     "right_to_compensation": true,
-     "possibility_of_appeal": true,
-     "referenced_paragraphs": ["§ 23", "§ 248 odst. 2 písm. e) občanského soudního řádu", "§ 46 odst. 1 písm. d) s. ř. s.", "§ 244 odst. 2 o. s. ř."],
-     "referenced_entities": ["SPORT WORKS, s. r. o.", "Ing. Jan Prokop", "Krajský soud v Brně"]
-}}
-"""
+    Carefully analyse it and answer the following questions.
+    1. what was the verdict
+    2. is there a right to compensation of legal expenses
+    3. is there a possibility of appeal
+    4. list of referenced paragraphs
+    5. list of referenced entities
+    Output the answer as a single valid JSON only, do not output any other text or characters. Example of output:
+    {{
+         "verdict": "žaloba odmítnuta",
+         "right_to_compensation": true,
+         "possibility_of_appeal": true,
+         "referenced_paragraphs": ["§ 23", "§ 248 odst. 2 písm. e) občanského soudního řádu", "§ 46 odst. 1 písm. d) s. ř. s.", "§ 244 odst. 2 o. s. ř."],
+         "referenced_entities": ["SPORT WORKS, s. r. o.", "Ing. Jan Prokop", "Krajský soud v Brně"]
+    }}
+    """
 
-input_prompts = []
+    # input_prompts = []
 
-print(f"Opening {input_dir}...")
-for c, file in enumerate(os.listdir(input_dir)):
-    # print(f"Opening {file}...")
-    with open(f"{input_dir}/{file}", encoding="utf-8") as f:
-        content = f.read()
+    print(f"Opening {input_dir}...")
 
-    full_prompt = prompt_template.format(document_contents=content.strip())
-    if len(full_prompt) > MAX_MODEL_LEN:
-        print(f"File {file} as it is too long ({len(full_prompt)} characters).")
-        content_shortened = content[:MAX_MODEL_LEN - len(prompt_template) - 100]
-        full_prompt = prompt_template.format(document_contents=content_shortened.strip())
-    input_prompts.append(full_prompt)
+    error_count = 0
+    total_count = 0
+    print(f"Generating outputs to {output_file}...")
+    # TODO change mode to append file?
+    with open(output_file, "w", encoding="utf-8") as out_f:
+        # for prompt in tqdm(input_prompts):
+        previous_iteration_start = datetime.datetime.now()
+        # for file in tqdm(os.listdir(input_dir)[(585+1+453+1+376+1+1500):]):
+        for file in tqdm(os.listdir(input_dir)):
+            print(f"Opening {file}...")
+            with open(f"{input_dir}/{file}", encoding="utf-8") as f:
+                content = f.read()
 
-# get a subset of 10 prompts
-# input_prompts = input_prompts[:10]
-input_prompts = input_prompts
+            full_prompt = prompt_template.format(document_contents=content.strip())
 
-print(f"Generating outputs for {len(input_prompts)} prompts...")
-outputs = llm.generate(input_prompts, sampling_params, use_tqdm=True)
-print(f"Generated {len(outputs)} outputs.")
+            if len(full_prompt) > CONTEXXT_MAX_LENGTH:
+                print(f"File {file} as it is too long ({len(full_prompt)} characters).")
+                content_shortened = content[:CONTEXXT_MAX_LENGTH - len(prompt_template) - 100]
+                full_prompt = prompt_template.format(document_contents=content_shortened.strip())
+            prompt = full_prompt
 
-error_count = 0
-print(f"Writing outputs to {output_file}...")
-with open(output_file, "w", encoding="utf-8") as out_f:
-    for output in outputs:
-        txt = output.outputs[0].text.strip()
-        if '```json' in txt:
+            entry = {
+                "file": file,
+                "prompt": prompt,
+            }
+
+            iteration_start = datetime.datetime.now()
+            diff = iteration_start - previous_iteration_start
+            # print(diff)
+            # print(diff.total_seconds())
+
+            if diff.total_seconds() <= 4.5:
+                sleep_time = 4.5 - diff.total_seconds()
+                # print(f"Sleeping for {sleep_time} seconds...")
+                sleep(ceil(sleep_time))
+
+            response = llm.invoke(prompt)
+
+            txt = response.content.strip()
+            if '```json' in txt:
                 txt = txt.replace('```json', '').replace('```', '')
-        try:
-            entry = json.loads(txt)
-        except:
-            print(f"Error parsing JSON: {txt[:100]}")
-            # wrap string in JSON
-            entry = {"error": txt}
-            error_count += 1
-        out_f.write(json.dumps(entry, ensure_ascii=False) + "\n")
+            try:
+                entry["response"] = json.loads(txt)
+            except:
+                print(f"Error parsing JSON: {txt[:100]}")
+                entry["response"] = None
+                entry["error"] = txt
+                error_count += 1
+            out_f.write(json.dumps(entry, ensure_ascii=False) + "\n")
+            previous_iteration_start = iteration_start
+        total_count += 1
 
-print(f"Done. {error_count} errors encountered. % of errors: {error_count/len(outputs)*100:.2f}%")
+    print(f"Done. {error_count} errors encountered. % of errors: {error_count / total_count * 100:.2f}%")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Process NSS documents using a specified LLM.")
+    parser.add_argument("--input_dir", help="Directory containing input documents.")
+    parser.add_argument("--output_file", help="Path to the output JSONL file.")
+    # parser.add_argument("model_path", help="Path to the LLM model.")
+
+    args = parser.parse_args()
+
+    # process_nss_documents(args.input_dir, args.output_file, args.model_path)
+    process_nss_documents(args.input_dir, args.output_file)
